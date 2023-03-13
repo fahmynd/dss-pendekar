4df8a88ff3224b084ec49c32216407f9406a0cbb
Fahmy Nd <travianerst12@gmail.com>
2023-03-13 15:52:31 +0800

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -225,26 +225,3 @@ $(document).ready(function () {
     effect: 'slide-left'
   });
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  new Chart(document.querySelector('#potensiPieChart'), {
-    type: 'pie',
-    data: {
-      labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-      ],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }]
-    }
-  });
-});
