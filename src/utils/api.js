028e0210da0710e3362a17e6028e269afb43aa31
Fahmy Nd <travianerst12@gmail.com>
2023-03-23 14:04:58 +0800

    pendekarV1

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
 export const KODE_SLUG = window.location.hostname.split('.')[0]
-export const BASE_API_URL = 'https://pendekar.digitaldesa.id/api/'
+export const BASE_API_URL = `https://${KODE_SLUG}.pendekar.digitaldesa.id/api/`
