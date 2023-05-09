8c6060fa6999ac38b24be3341df5540d0ca385e0
Fahmy Nd <travianerst12@gmail.com>
2023-05-09 09:17:55 +0800

    filter bansos

---
+++ src/component/filterBansos.js
@@ -0,0 +1,126 @@
+import React, { useMemo, useState, Fragment } from "react";
+import Bantuan from '../../src/assets/icon/mendapatBantuan.svg'
+import KK from '../../src/assets/icon/kk.svg'
+
+const FilterBansos = (props) => {
+    const { list_kecamatan, list_desa } = props.resultData.data;
+
+    const [selectedKec, setSelectedKec] = useState("")
+    const [selectedDesa, setSelectedDesa] = useState("")
+
+    const listDesa = useMemo(() => {
+        setSelectedDesa("")
+        if (selectedKec) {
+            return list_desa.filter(d => {
+                return d.kode_wilayah.startsWith(selectedKec)
+            })
+        }
+        return []
+    }, [list_desa, selectedKec])
+
+
+    const bansos = useMemo(() => {
+        let individu = 0;
+        let keluarga = 0;
+
+        if (selectedKec && selectedDesa) {
+            const deskel = listDesa.find(d => d.kode_wilayah === selectedDesa)
+            if (deskel) {
+                individu = parseInt(deskel.jml_penerima)
+                keluarga = parseInt(deskel.jml_penerima_kk)
+            }
+        } else {
+            if (selectedKec) {
+                listDesa.forEach(desa => {
+                    individu += parseInt(desa.jml_penerima);
+                    keluarga += parseInt(desa.jml_penerima_kk);
+                })
+            } else {
+                list_desa.forEach(d => {
+                    individu += parseInt(d.jml_penerima);
+                    keluarga += parseInt(d.jml_penerima_kk);
+                })
+            }
+        }
+
+        return {
+            individu,
+            keluarga
+        }
+    }, [listDesa, selectedDesa])
+
+    // console.log(bansos.individu);
+
+    return (
+        <Fragment>
+            <div className="col-lg-12">
+                <div className="card">
+                    <div className="card-body">
+                        <div className="filter-primary">
+                            <button type="button" className="btn btn-primary">Export Report</button>
+                        </div>
+                        <div className="row g-1 my-4">
+                            <div className="col-3">
+                                <select onChange={e => setSelectedKec(e.target.value)} className="form-select" aria-label="Default select example">
+                                    <option value={''}>Semua Kecamatan</option>
+                                    {list_kecamatan.map((item) => {
+                                        return (
+                                            <option key={item.kode_wilayah} value={item.kode_wilayah} selected={selectedKec === item.kode_wilayah}>{item.nama_kecamatan}</option>
+                                        )
+                                    })}
+                                </select>
+                            </div>
+                            <div className="col-3">
+                                <select onChange={e => setSelectedDesa(e.target.value)} className="form-select" aria-label="Default select example">
+                                    <option value={''}>Semua Desa</option>
+                                    {listDesa.map((item) => {
+                                        return (
+                                            <option key={item.kode_wilayah} value={item.kode_wilayah} selected={selectedDesa === item.kode_wilayah}>{item.nama_deskel}</option>
+                                        )
+                                    })}
+                                </select>
+                            </div>
+
+                        </div>
+
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <div className="card-body rounded-2 bg-primary">
+                                        <div className="row px-4 py-2">
+                                            <div className="col-6">
+                                                <span className="jum-bantuan timer count-title count-number">{bansos.individu}</span> <br />
+                                                <span className="judul-card-bansos">Penduduk Mendapat Bantuan</span>
+                                            </div>
+                                            <div className="col-6 text-right center-v">
+                                                <img src={Bantuan} width="60" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="card">
+                                    <div className="card-body rounded-2 bg-secondary">
+                                        <div className="row px-4 py-2">
+                                            <div className="col-6">
+                                                <span className="jum-bantuan timer count-title count-number">{bansos.keluarga}</span> <br />
+                                                <span className="judul-card-bansos">Kepala Keluarga Mendapat Bantuan</span>
+                                            </div>
+                                            <div className="col-6 text-right center-v">
+                                                <img src={KK} width="60" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default FilterBansos;
--- src/screens/bantuanSosial.js
+++ src/screens/bantuanSosial.js
@@ -6,6 +6,7 @@ import Bantuan from '../../src/assets/icon/mendapatBantuan.svg'
 import KK from '../../src/assets/icon/kk.svg'
 import BansosTable from '../component/datatable/RekapBansosDataTable';
 import { formatAngka, format_tgl } from '../utils/helper.min';
+import FilterBansos from '../component/filterBansos'
 
 const BantuanSosial = () => {
     const [resultData, setResultData] = useState();
@@ -59,79 +60,7 @@ const BantuanSosial = () => {
                 <section className="section dashboard">
                     <div className="row">
 
-                        <div className="col-lg-12">
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="filter-primary">
-                                        <button type="button" className="btn btn-primary">Export Report</button>
-                                    </div>
-                                    <div className="row g-1 my-4">
-                                        <div className="col-3">
-                                            <select defaultValue='0' className="form-select" aria-label="Pilih Kecamatan">
-                                                <option value='0'>Semua Kecamatan</option>
-                                                {kec.map((item) => {
-                                                    return (
-                                                        <SelectOptions
-                                                            key={item.kode_wilayah}
-                                                            value={item.kode_wilayah}
-                                                            title={item.nama_kecamatan}
-                                                        />
-                                                    )
-                                                })}
-                                            </select>
-                                        </div>
-                                        <div className="col-3">
-                                            <select defaultValue='0' className="form-select" aria-label="Pilih Desa">
-                                                <option value='0'>Semua Desa</option>
-                                                {desa.map((item) => {
-                                                    return (
-                                                        <SelectOptions
-                                                            key={item.kode_wilayah}
-                                                            value={item.kode_wilayah}
-                                                            title={item.nama_deskel}
-                                                        />
-                                                    )
-                                                })}
-                                            </select>
-                                        </div>
-                                    </div>
-
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <div className="card">
-                                                <div className="card-body rounded-2 bg-primary">
-                                                    <div className="row px-4 py-2">
-                                                        <div className="col-6">
-                                                            <span className="jum-bantuan timer count-title count-number">{formatAngka(individu)}</span> <br />
-                                                            <span className="judul-card-bansos">Penduduk Mendapat Bantuan</span>
-                                                        </div>
-                                                        <div className="col-6 text-right center-v">
-                                                            <img src={Bantuan} width="60" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="card">
-                                                <div className="card-body rounded-2 bg-secondary">
-                                                    <div className="row px-4 py-2">
-                                                        <div className="col-6">
-                                                            <span className="jum-bantuan timer count-title count-number">{formatAngka(keluarga)}</span> <br />
-                                                            <span className="judul-card-bansos">Kepala Keluarga Mendapat Bantuan</span>
-                                                        </div>
-                                                        <div className="col-6 text-right center-v">
-                                                            <img src={KK} width="60" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
+                        {resultData && <FilterBansos resultData={resultData} />}
 
                         <div className="col-lg-12">
                             <div className="card">
@@ -150,12 +79,6 @@ const BantuanSosial = () => {
 
             </main>
         </Fragment>
-    )
-}
-
-function SelectOptions(props) {
-    return (
-        <option value={props.value}>{props.title}</option>
     )
 }
 
