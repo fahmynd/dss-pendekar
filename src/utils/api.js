2f3ea199154b9691d6ccf02e9844654531d33a26
Ito Rijal <ophyeq@gmail.com>
2023-05-25 07:08:27 +0800

    fix: dynamic subdomain

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
-// export const KODE_SLUG = window.location.hostname.split('.')[0]
-export const KODE_SLUG = `sulselprov-enrekangkab`
+export const KODE_SLUG = window.location.hostname.split('.')[0]
+// export const KODE_SLUG = `enrekangkab`
 export const BASE_API_URL = `https://${KODE_SLUG}.pendekar.digital/api/`
 
 const TOKEN_KEY = "api_token"
 
 export function setApiToken(token) {
-    localStorage.setItem(TOKEN_KEY,token)
+    localStorage.setItem(TOKEN_KEY, token)
 }
 
 export function getApiToken() {
